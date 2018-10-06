451c2c34ce0c3c7eb56b2b8993f182a006e9f1ff
yesrolya <yesrolya@yandex.ru>
2018-10-07 02:32:08 +1000

    1

---
+++ list3.py
@@ -0,0 +1,21 @@
+# Дан список чисел.
+# Выведите значение наибольшего элемента в списке, а затем индекс этого элемента в списке.
+# Если наибольших элементов несколько, выведите индекс первого из них.
+
+lst = input('Input space-separated list of numbers: ')
+lst = lst.split(' ')
+
+max_index: int = 0
+try:
+    for l in lst:
+        if float(lst[max_index]) < float(l):
+            max_index = lst.index(l)
+
+    print('Value: ', lst[max_index], '\nIndex: ', max_index)
+
+except ValueError:
+    print('Could not convert string in list to float')
+except IndexError:
+    print('List is empty')
+except TypeError:
+    print('List is empty')
